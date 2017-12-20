628bdf858b25492a3e94c1ce14b9f1613a5a9b5c
MingHao Juan <howardjuan0118@gmail.com>
2017-12-20 17:45:40 +0800

    finish 2016 heatmap

--- bundle.js
+++ bundle.js
@@ -7928,7 +7928,6 @@ function initMap() {
     center: new google.maps.LatLng(23.6,121),
     mapTypeId: 'terrain'
   });
-  // Fuck government. They use different formats among data.
   for (var i = 0; i <= 5; i++) {
     Papa.parse(`./src/data/2016/${i}.csv`, {
       download: true,
@@ -7936,9 +7935,12 @@ function initMap() {
         console.log("Parsing complete:", results, file);
         var fileId = file[16];
         var data = results.data;
-        for (var j = 7; j < data.length; j += 2) {
+        var heatmapData = [];
+        for (var j = 7; j + 1 < data.length; j += 2) {
+          if (data[j].length <= 4 || data[j + 1].length <= 4) continue;
           var lat = data[j + 1][4], lng = data[j][4];
           if (lat.trim().length === 0 || lng.trim().length === 0) continue;
+          // Fuck government. They use different formats and coord systems among data.
           switch (fileId) {
             case '0':
             case '1':
@@ -7955,20 +7957,38 @@ function initMap() {
               lng = lng.slice(1);
               break;
             case '5':
+              // console.log(lat, lng);
+              if (Number(lat) < 1000000) { // err handling
+                var temp = lat;
+                lat = lng;
+                lng = temp;
+                break;
+              }
               Proj4.defs('EPSG:3826', '+title=TWD97 +proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=GRS80 +no_defs');
               var lngLat = Proj4('EPSG:3826', 'EPSG:4326', [Number(lng), Number(lat)]);
               lat = lngLat[1];
               lng = lngLat[0];
+              if (lng > 140) lng -= 20; // err handling
               break;
             default:
           }
           var latLng = new google.maps.LatLng(lat, lng);
-          // console.log([latLng.lat(), latLng.lng()]);
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
+          var weightedLocation = {
+            location: latLng,
+            weight: Number(data[j][17].trim().split(',').join(''))
+          };
+          heatmapData.push(weightedLocation);
+          // console.log([latLng.lat(), latLng.lng()], weightedLocation, data[j][17].trim());
+          // var marker = new google.maps.Marker({
+          //   position: latLng,
+          //   map: map
+          // });
         }
+        var heatmap = new google.maps.visualization.HeatmapLayer({
+          data: heatmapData,
+          radius: 30,
+          map: map
+        });
       }
     });
   }
